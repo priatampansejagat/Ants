771f4d5dd90b935c5bb135627ee77903c8fc8ff2
jenderalsemut <62284026+priatampansejagat@users.noreply.github.com>
2023-05-15 18:02:58 +0700

    ui done

--- view/Home.py
+++ view/Home.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 
 class Home():
 
@@ -8,6 +9,14 @@ class Home():
 
     def __init__(self, controllers):
         controllers = controllers
+
+        # Undefined UI
+        global button_remove_rule
+        self.button_remove_rule = {}
+
+        global list_rule
+        self.list_rule = {}
+
 
         # CREATE UI
         main_window = tk.Tk(screenName='main_screen', baseName='main_basename', className='Ants', useTk=1)
@@ -26,18 +35,22 @@ class Home():
         label_url.grid(row=0, column=0)
 
         input_url = tk.Entry(frame_input, width=100)
-        input_url.grid(row=0, column=1, pady=10, padx=10)
+        input_url.grid(row=0, column=1, pady=20, padx=10)
 
         button = tk.Button(frame_input, text='Dump', width=25, command=lambda:self.data_dump(str(input_url.get())))
         button.grid(row=0, column=2)
 
         global text_dump
-        self.text_dump = tk.Text(frame_input, height=40, width=110)
-        self.text_dump.grid(row=1,column=0,columnspan=3,pady=10)
+        self.text_dump = tk.Text(frame_input, height=38, width=110, wrap=tk.NONE)
+        self.text_dump.grid(row=1,column=0,columnspan=3)
 
-        text_dump_scrollbar = tk.Scrollbar(frame_input, command=self.text_dump.yview)
-        text_dump_scrollbar.grid(row=1, column=3, sticky='nsew')
-        self.text_dump['yscrollcommand'] = text_dump_scrollbar.set
+        text_dump_scrollbar_y = tk.Scrollbar(frame_input, command=self.text_dump.yview)
+        text_dump_scrollbar_y.grid(row=1, column=3, sticky='nsew')
+        self.text_dump['yscrollcommand'] = text_dump_scrollbar_y.set
+
+        text_dump_scrollbar_x = tk.Scrollbar(frame_input, command=self.text_dump.xview, orient='horizontal')
+        text_dump_scrollbar_x.grid(row=2, column=0,columnspan=3, sticky='nsew')
+        self.text_dump['xscrollcommand'] = text_dump_scrollbar_x.set
 
         frame_extract = tk.Frame(frame_input, padx=30, pady=15, highlightbackground="grey", highlightthickness=1)
         frame_extract.grid(row=0, column=5, rowspan=2)
@@ -49,16 +62,37 @@ class Home():
         text_grab = tk.Entry(frame_extract, width=40)
         text_grab.grid(row=1, column=0)
 
-        button_grab_data = tk.Button(frame_extract, text='Add Rule', command=lambda:self.grab_data(str(text_grab.get())))
-        button_grab_data.grid(row=1, column=1)
+        button_add_rule = tk.Button(frame_extract, text='Add Rule', command=lambda:self.add_rules(str(text_grab.get())))
+        button_add_rule.grid(row=1, column=1)
 
+        frame_extract_options = tk.Canvas(frame_extract)
+        frame_extract_options.grid(row=2, column=0, pady=10,columnspan=2)
 
-        frame_extract_options = tk.Frame(frame_extract)
-        frame_extract_options.grid(row=2, column=0)
+        global canvas_extract_options
+        self.canvas_extract_options = tk.Canvas(frame_extract_options, width=280, height=480, background='white')
+        self.canvas_extract_options.grid(row=0, column=0)
 
-        """
-        tinggal tambah list rule sama tombol execute extract
-        """
+        global rule_items_nest
+        self.rule_items_nest=tk.Frame(self.canvas_extract_options,background='white')
+        # self.rule_items.grid(row=0,column=0)
+        self.canvas_extract_options.create_window((0, 0), window=self.rule_items_nest, anchor='nw')
+
+        global list_rule_scroll
+        self.list_rule_scroll = tk.Scrollbar(frame_extract_options,orient="vertical", command=self.canvas_extract_options.yview)
+        self.list_rule_scroll.config(command=self.canvas_extract_options.yview)
+        self.list_rule_scroll.grid(row=0, column=1, sticky='nsew')
+        self.canvas_extract_options['yscrollcommand'] = self.list_rule_scroll.set
+
+        # self.canvas_extract_options.configure(yscrollcommand=list_rule_scroll.set, scrollregion="0 0 0 %s" % self.rule_items_nest.winfo_height())
+
+        frame_button_extraction_action = tk.Frame(frame_extract)
+        frame_button_extraction_action.grid(row=3, column=0,columnspan=2)
+
+        button_extraction = tk.Button(frame_button_extraction_action, text='Extract', command=lambda:self.extraction(), background='skyblue')
+        button_extraction.grid(row=0, column=1)
+
+        button_clear = tk.Button(frame_button_extraction_action, text='Clear', command=lambda: self.clear_rules_all(), background='pink')
+        button_clear.grid(row=0, column=0,padx=5)
 
         menu = tk.Menu(main_window)
         main_window.config(menu=menu)
@@ -79,33 +113,60 @@ class Home():
         else:
             print('no url')
 
-    def grab_data(self,value='no url'):
+    def add_rules(self,value='no rules'):
         if len(value.strip(' ')) > 0:
-            print(value)
+            self.list_rule[str(value)] = tk.Label(self.rule_items_nest, text=str(value),background='white')
+            self.list_rule[str(value)].grid(row=len(self.list_rule),column=0)
+            # self.list_rule[str(value)].pack()
+
+            # self.button_remove_rule[str(value)] = tk.Button(self.rule_items_nest, text='del', background='red', command=lambda : self.remove_rule(str(value)))
+            # self.button_remove_rule[str(value)].grid(row=len(self.list_rule),column=1)
+
+            self.rule_items_nest.update()
+            self.canvas_extract_options['scrollregion'] = "0 0 0 %s" % self.rule_items_nest.winfo_height()
         else:
             print('no url')
 
-    def remove_datagrab(self,value='no url'):
+    """
+    def remove_rule(self,value='no url'):
         if len(value.strip(' ')) > 0:
-            print(value)
+            self.list_rule[str(value)].destroy()
+            self.button_remove_rule[str(value)].destroy()
+            del self.list_rule[str(value)]
+            del self.button_remove_rule[str(value)]
+
+            print('remove {}'.format(self.list_rule))
+
+            self.rule_items_nest.update()
+            self.canvas_extract_options['scrollregion'] = "0 0 0 %s" % self.rule_items_nest.winfo_height()
         else:
             print('no url')
+    """
+
+    def clear_rules_all(self):
+        print('removing all rules')
+        if len(self.list_rule) > 0:
+            for child in self.rule_items_nest.winfo_children():
+                child.destroy()
+
+        else:
+            print('No Rules')
+            print (self.list_rule)
 
 
+        self.list_rule.clear()
+
+        self.rule_items_nest.update()
+        self.canvas_extract_options['scrollregion'] = "0 0 0 %s" % self.rule_items_nest.winfo_height()
+
+    def extraction(self):
+        # for key in self.list_rule:
+        #     print(key)
+        if len(self.list_rule)>0:
+            print('hiolalala')
+        else:
+            print('Please add rules')
 
 
-
-
-
-
-
-#  UI DESIGN
-class UI(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-
-
-
-        pass
 
 
